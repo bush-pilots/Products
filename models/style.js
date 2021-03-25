31f3f6b4886de5a20b1604dea23f4ac4b495508e
Mason Hoffman <mhoffman3939@gmail.com>
2021-03-25 16:13:50 -0700

    Added loader.io code to get routes

--- models/style.js
+++ models/style.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const styleSchema = Schema({
-  style_id: Number,
   product_id: Number,
-  style_name: String,
-  sale_price: Number,
-  original_price: Number,
-  default: Boolean,
+  results: [{
+    style_id: Number,
+    style_name: String,
+    sale_price: Number,
+    original_price: Number,
+    default: Boolean,
+  }]
   },
   {
     collection: 'Styles'
